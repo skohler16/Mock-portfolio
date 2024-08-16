e23b2c173641556ca7205a12bd039432bbe4498b
Scooter C Kohler <skohler16@gmail.com>
2024-08-15 21:48:56 -0700

    added some variables to make my server flexable

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route("/<username>/<int:post_id>")
+def hello_world(username = None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
 
 @app.route("/blog")
 def blog():
     return "<p>Hello, Stink Blog face!</p>"
+
 @app.route("/about_me.html")
 def about():
     return render_template('about_me.html')
