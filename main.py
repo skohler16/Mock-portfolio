438764672930f95891edd1e95800e7a8b5ce98ad
Scooter C Kohler <skohler16@gmail.com>
2024-08-19 21:16:15 -0700

    created a csv route and form for submitting and storing form data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 print(__name__)
@@ -10,11 +11,25 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\nEmail:{email}, Subject:{subject}, Message: {message}')
+
+def write_to_CSV(data):
+    with open('database.csv', mode='a') as csv_database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(csv_database)
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'uhhh... ya messed up fool'
