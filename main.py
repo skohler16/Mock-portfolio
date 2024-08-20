075fe068e2514cb3b89350fd9a4e51db94109da2
Scooter C Kohler <skohler16@gmail.com>
2024-08-19 20:35:45 -0700

    made contact form work correctly, added a thankyou message

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
+print(__name__)
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'uhhh... ya messed up fool'
